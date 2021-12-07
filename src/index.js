254d2145e4bf8c745075b4e8f60e7ccad73e065c
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-07 11:37:34 +0300

    feat: implement required functionality

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix === undefined || matrix.length === 0) {
+    return [];
+  }
+  const toOddReversed = (row, idx) => idx % 2 ? row.reverse() : row;
+  return matrix
+    .map(toOddReversed)
+    .flat();
 }
